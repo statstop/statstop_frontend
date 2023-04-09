81765682c86ca0a7208e2089cbcae3a6c795a9dd
Hansal21 <hansaldhandha123@gmail.com>
2023-04-09 22:25:25 +0530

    Added Dockerfile

---
+++ src/components/CricketScorecard.js
@@ -0,0 +1,15 @@
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Table from './Table'
+function CricketScorecard() {
+  return (
+   <>
+    <Table>
+      
+    </Table>
+   </>
+  );
+}
+
+export default CricketScorecard;
