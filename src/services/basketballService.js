63c79d6c0ec09d3fd4900551c7b635aa986a1991
Hansal21 <hansaldhandha123@gmail.com>
2023-05-09 18:35:06 +0530

    Modified Ip

--- src/services/basketballService.js
+++ src/services/basketballService.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 const getGames = async () =>{
-    const response = await axios.get(`http://localhost:8085/basketball/get-games`)
+    const response = await axios.get(`http://${BE_IP}:8085/basketball/get-games`)
     return response.data
 }
 const getTeamsAdmin = async () =>{
-    const response = await axios.get(`http://localhost:8085/basketball/get-teams`)
+    const response = await axios.get(`http://${BE_IP}:8085/basketball/get-teams`)
     return response.data
 }
 const addTeam = async (newTeam) =>{
-    const response = await axios.post(`http://localhost:8085/basketball/add-team`,newTeam)
+    const response = await axios.post(`http://${BE_IP}:8085/basketball/add-team`,newTeam)
     return response.data
 }
 const addGame = async (newGame) =>{
-    const response = await axios.post(`http://localhost:8085/basketball/add-game`,newGame)
+    const response = await axios.post(`http://${BE_IP}:8085/basketball/add-game`,newGame)
     return response.data
 }
 const updateGame = async (gameId,teamId,points) =>{
-    const response = await axios.post(`http://localhost:8085/basketball/update?gameId=${gameId}&teamId=${teamId}&points=${points}`,updateGame)
+    const response = await axios.post(`http://${BE_IP}:8085/basketball/update?gameId=${gameId}&teamId=${teamId}&points=${points}`,updateGame)
     return response.data
 }
 
